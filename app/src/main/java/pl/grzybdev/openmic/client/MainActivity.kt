7f45918f9ab36ba169f1785e87d6a5c5f75e2886
Marek Grzyb <grzybdev@gmail.com>
2021-06-21 13:02:23 +0200

    Started working on MainActivity behaviour

--- app/src/main/java/pl/grzybdev/openmic/client/MainActivity.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/MainActivity.kt
@@ -2,10 +2,22 @@ package pl.grzybdev.openmic.client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.submitBtn);
+        button.setOnClickListener { connectToPC(); }
+    }
+
+    fun connectToPC() {
+        button.isEnabled = false;
+        button.text = getString(R.string.btn_connecting);
     }
 }
