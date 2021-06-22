b185e1488bad1b5dc16c9baa9351a7faac498635
Marek Grzyb <grzybdev@gmail.com>
2021-06-22 21:14:42 +0200

    Started working on network communication

--- app/src/main/java/pl/grzybdev/openmic/client/MainActivity.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/MainActivity.kt
@@ -3,6 +3,11 @@ package pl.grzybdev.openmic.client
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +24,14 @@ class MainActivity : AppCompatActivity() {
     fun connectToPC() {
         button.isEnabled = false;
         button.text = getString(R.string.btn_connecting);
+
+        val ip: EditText = findViewById(R.id.ipAddress);
+        val port: EditText = findViewById(R.id.port);
+
+        val thread = Thread {
+            val socket: Socket = Socket(ip.text.toString(), Integer.parseInt(port.text.toString()));
+            val outToServer: DataOutputStream = DataOutputStream(socket.getOutputStream());
+            val inFromServer: BufferedReader = BufferedReader(InputStreamReader(socket.getInputStream()));
+        }.start();
     }
 }
