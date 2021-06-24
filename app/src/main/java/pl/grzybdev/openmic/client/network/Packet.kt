63a94f37347ebfa8bf7cbe800924a77a7724c818
Marek Grzyb <grzybdev@gmail.com>
2021-06-24 23:07:34 +0200

    Added device and app metadata to hello packet

--- app/src/main/java/pl/grzybdev/openmic/client/network/Packet.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/network/Packet.kt
@@ -1,5 +1,6 @@
 package pl.grzybdev.openmic.client.network
 
+import pl.grzybdev.openmic.client.BuildConfig
 import java.lang.StringBuilder
 
 class Packet {
@@ -11,6 +12,16 @@ class Packet {
             val finalStr: StringBuilder = StringBuilder(magicValue);
             finalStr.append(seperator);
             finalStr.append(ClientPacketType.CLIENT_HELLO.value);
+            finalStr.append(seperator);
+            finalStr.append(BuildConfig.VERSION_NAME + "\n" + BuildConfig.VERSION_CODE);
+            finalStr.append(seperator);
+            finalStr.append(BuildConfig.DEBUG);
+            finalStr.append(seperator);
+            finalStr.append("Android\n" + android.os.Build.VERSION.RELEASE);
+            finalStr.append(seperator);
+            finalStr.append(android.os.Build.MANUFACTURER + "\n" + android.os.Build.MODEL);
+            finalStr.append(seperator);
+            finalStr.append(android.os.Build.FINGERPRINT);
             finalStr.append(seperator);
 
             return finalStr.toString().toByteArray();
