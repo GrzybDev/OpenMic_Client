4a80591cee3c505ae49c1ac736d4fc5442454fb5
Marek Grzyb <grzybdev@gmail.com>
2021-06-25 11:52:45 +0200

    Finished Proof of Concept Client

--- app/src/main/java/pl/grzybdev/openmic/client/MainActivity.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/MainActivity.kt
@@ -1,13 +1,20 @@
 package pl.grzybdev.openmic.client
 
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import pl.grzybdev.openmic.client.network.Packet
 import java.io.BufferedReader
 import java.io.DataOutputStream
 import java.io.InputStreamReader
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
 import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
@@ -36,6 +43,27 @@ class MainActivity : AppCompatActivity() {
 
             outToServer.write(Packet.getHelloPacket());
             outToServer.flush();
+
+            val connectPacket: String = inFromServer.readLine();
+            val data = connectPacket.split(0.toChar());
+
+            val recorded: AudioRecord = AudioRecord(MediaRecorder.AudioSource.MIC,48000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,
+            AudioRecord.getMinBufferSize(48000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT));
+
+            recorded.startRecording();
+
+            Thread {
+                val bufSize = AudioRecord.getMinBufferSize(48000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+                val buf: ByteArray = ByteArray(bufSize);
+                val addr = InetAddress.getByName(ip.text.toString());
+                val sock = DatagramSocket();
+
+                while (true) {
+                    recorded.read(buf, 0, bufSize);
+                    val packet = DatagramPacket(buf, bufSize, addr, Integer.parseInt(data[1]));
+                    sock.send(packet);
+                }
+            }.start();
         }.start();
     }
 }
