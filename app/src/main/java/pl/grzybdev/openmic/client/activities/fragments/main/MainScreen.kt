f820ab806ce31c6f37f21be9b1e85cd0167d24e5
Marek Grzyb <grzybdev@gmail.com>
2021-08-01 17:45:45 +0200

    Updated MainScreen to use FragmentInterface

--- app/src/main/java/pl/grzybdev/openmic/client/activities/fragments/main/MainScreen.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/activities/fragments/main/MainScreen.kt
@@ -10,26 +10,20 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
 import pl.grzybdev.openmic.client.R
-import pl.grzybdev.openmic.client.enums.ConnectionType
+import pl.grzybdev.openmic.client.enums.manager.ConnectionType
+import pl.grzybdev.openmic.client.interfaces.ui.FragmentInterface
 
 
-class MainScreen : Fragment() {
+class MainScreen : Fragment(), FragmentInterface {
 
-    private val adViewsPortrait: MutableList<Int> = mutableListOf(
-        R.id.adView_Main_Top,
-        R.id.adView_Main_Bottom
-    )
-
-    private val adViewsLand: MutableList<Int> = mutableListOf(R.id.adView_Main_Big)
+    override val adViewsPortrait: List<Int>
+        get() = listOf(R.id.adView_Main_Top, R.id.adView_Main_Bottom)
+    override val adViewsLandscape: List<Int>
+        get() = listOf(R.id.adView_Main_Big)
 
     private lateinit var wifiStatus: TextView
     private lateinit var usbStatus: TextView
-
-    private var wifiLastStatus: Boolean = false
-    private var usbLastStatus: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,82 +41,27 @@ class MainScreen : Fragment() {
         Log.d(javaClass.name, "onStart: MainScreen has been started")
 
         initViews()
-        initAds()
+        initAds(this)
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
 
-        Log.d(javaClass.name, "onConfigurationChanged")
-        initAds()
+        Log.d(javaClass.name, "onConfigurationChanged: Reloading ads...")
+        initAds(this)
     }
 
-    private fun initViews() {
+    override fun initViews() {
         Log.d(javaClass.name, "initViews: Initializing views...")
 
         wifiStatus = requireView().findViewById(R.id.statusWifi)
         usbStatus = requireView().findViewById(R.id.statusUSB)
     }
 
-    private fun initAds() {
-        Log.d(javaClass.name, "initAds: Reloading ads in MainScreen...")
-
-        val orientation = this.resources.configuration.orientation
-        val adViews: List<Int> = if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            adViewsPortrait
-        } else {
-            adViewsLand
-        }
-
-        adViews.forEach { adViewID ->
-
-            run {
-                val adRequest = AdRequest.Builder().build()
-                val adView = requireView().findViewById<AdView>(adViewID)
-
-                adView.loadAd(adRequest)
-            }
-        }
-    }
-
-    fun updateListenerStatus(connectionType: ConnectionType, isConnected: Boolean) {
+    fun updateListenerStatus(connectionType: ConnectionType, textColor: Int, text: String) {
         val statusTextView: TextView = when (connectionType) {
-            ConnectionType.WiFi -> wifiStatus
-            ConnectionType.USB -> usbStatus
-        }
-
-        val changeText: String = when (connectionType) {
-            ConnectionType.WiFi -> "Wi-Fi"
-            ConnectionType.USB -> "USB"
-        }
-
-        Log.d(javaClass.name, "updateListenerStatus: Changing status of $changeText to ${if (isConnected) "enabled" else "disabled"}")
-
-        val textColor: Int
-        val text: Int
-
-        if (statusTextView == wifiStatus) {
-            if (wifiLastStatus == isConnected) return
-            else wifiLastStatus = isConnected
-
-            if (isConnected) {
-                textColor = R.color.main_status_connected
-                text = R.string.main_status_wifi_enabled
-            } else {
-                textColor = R.color.main_status_not_connected
-                text = R.string.main_status_wifi_disabled
-            }
-        } else {
-            if (usbLastStatus == isConnected) return
-            else usbLastStatus = isConnected
-
-            if (isConnected) {
-                textColor = R.color.main_status_disabled
-                text = R.string.main_status_usb_connected
-            } else {
-                textColor = R.color.main_status_not_connected
-                text = R.string.main_status_usb_disconnected
-            }
+            ConnectionType.WiFi -> requireView().findViewById(R.id.statusWifi)
+            ConnectionType.USB -> requireView().findViewById(R.id.statusUSB)
         }
 
         requireActivity().runOnUiThread {
@@ -132,24 +71,7 @@ class MainScreen : Fragment() {
                 statusTextView.setTextColor(ContextCompat.getColor(requireContext(), textColor))
             }
 
-            statusTextView.text = getString(text)
-        }
-    }
-
-    fun setWifiDisabled() {
-        requireActivity().runOnUiThread {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                wifiStatus.setTextColor(requireContext().getColor(R.color.main_status_disabled))
-            } else {
-                wifiStatus.setTextColor(
-                    ContextCompat.getColor(
-                        requireContext(),
-                        R.color.main_status_disabled
-                    )
-                )
-            }
-
-            wifiStatus.text = getString(R.string.main_status_wifi_no_location)
+            statusTextView.text = text
         }
     }
 
