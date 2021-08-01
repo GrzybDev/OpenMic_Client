3f47e6f2b2045d8527a0489dd93b57f4d0c7b62b
Marek Grzyb <grzybdev@gmail.com>
2021-08-01 17:42:10 +0200

    Re-organised OpenMic Application

--- app/src/main/java/pl/grzybdev/openmic/client/OpenMic.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/OpenMic.kt
@@ -3,23 +3,21 @@ package pl.grzybdev.openmic.client
 import android.app.Application
 import com.google.android.gms.ads.MobileAds
 import com.google.firebase.analytics.FirebaseAnalytics
+import pl.grzybdev.openmic.client.enums.manager.ConnectionType
+import pl.grzybdev.openmic.client.managers.BaseManager
 import pl.grzybdev.openmic.client.network.BroadcastListener
 
 class OpenMic : Application() {
 
-    companion object {
-        lateinit var mFirebaseAnalytics: FirebaseAnalytics
-        lateinit var app: OpenMic
-    }
-
     val broadcastListener: BroadcastListener = BroadcastListener()
+
+    lateinit var connectionManagers: Map<ConnectionType, BaseManager>
+    lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate() {
         super.onCreate()
 
-        app = this
-
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
         MobileAds.initialize(this)
     }
 }
